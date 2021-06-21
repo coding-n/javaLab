bdcc8de9f72caa7a4b3bfa9e2461f517ad5c2f0d
eunjupark <httpej@naver.com>
2021-06-21 15:12:38 +0900

    2nd ver.

--- src/ThreadTest.java
+++ src/ThreadTest.java
@@ -1,9 +1,8 @@
 
-class MyThread implements Runnable{
-	
+class MyThread implements Runnable {
 	public void run() {
-		
-		for(int i = 0; i<=200; i++) {
+
+		for (int i = 0; i <= 200; i++) {
 			System.out.print(i + "\t");
 			try {
 				Thread.sleep(1);
@@ -17,27 +16,24 @@ class MyThread implements Runnable{
 
 
 public class ThreadTest {
-
 	public static void main(String[] args) {
 		System.out.println("start");
-		
+
 		Thread th1 = new Thread(new MyThread());
 		th1.start();
-		
+
 		MyThread mth = new MyThread();
 		Thread th2 = new Thread(mth);
 		th2.start();
-		
-		Runnable runnable =  new Runnable() {
+
+		Runnable runnable = new Runnable() {
 			public void run() {
 				System.out.println("Runnable Test");
 			}
 		};
-		
+
 		runnable.run();
-				
+
 		System.out.println("end");
 	}
 }
-
-
